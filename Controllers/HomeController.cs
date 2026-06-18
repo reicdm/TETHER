af615d5b71896f41236c23d831383b6c1d2cf6ea
Josiah Zachary Sy <zacharysy15@gmail.com>
2026-06-18 18:30:23 +0800

    Finished Landing Page, Initial Login Page, and global and auth css updated.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,5 +21,10 @@ namespace TETHER.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Login(string role)
+        {
+            return View();
+        }
     }
 }
