1ef08005c86dbfaf2a1e8aecc74886b404ab4e27
Johanna Estalilla <johannaestalilla@gmail.com>
2026-06-19 19:29:16 +0800

    DONE MODELS

---
+++ Models/TaskItemAssignment.cs
@@ -0,0 +1,13 @@
+﻿namespace TETHER.Models
+{
+    public class TaskItemAssignment
+    {
+        public int TaskId { get; set; }
+        public TaskItem? Task { get; set; }
+
+        public int AssignedTo { get; set; }
+        public TeamMember? TeamMember { get; set; }
+
+        public DateTime AssignedAt { get; set; }
+    }
+}
--- Models/TeamMember.cs
+++ Models/TeamMember.cs
@@ -5,11 +5,19 @@ namespace TETHER.Models
     public class TeamMember
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Role { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string MiddleName { get; set; }
+        public string LastName { get; set; } = string.Empty;
+
+        public int RoleId { get; set; }
+        public TeamMemberRole? Role { get; set; }
+
         public string PersonalGmail { get; set; } = string.Empty;
         public string SchoolGmail { get; set; } = string.Empty;
         public string GithubUsername { get; set; } = string.Empty;
-        public string? ProfileImageUrl { get; set; } = "/images/member-image/default.jpg";
+        public string ProfileImageUrl { get; set; } = "/images/member-image/default.jpg";
+
+        public int? PmId { get; set; }
+        public TeamMember? ProjectManager { get; set; }
     }
 }
